4527b81b2a863e19cfc27f41902da176ba19b91b
Graham Reeves <graham@grahamreeves.com>
2025-07-03 17:31:20 +0100

    Fixed stack heap erroring by making larger leaf sizes in kd tree

--- kdtree_tensor.hpp
+++ kdtree_tensor.hpp
@@ -17,7 +17,7 @@ struct PointsTensor {
 
     template <typename T>
     inline T *buildIndex() {
-        if (kdTree == nullptr) kdTree = static_cast<void *>(new T(3, *this, { 10 }));
+        if (kdTree == nullptr) kdTree = static_cast<void *>(new T(3, *this, { 100 }));
         return reinterpret_cast<T *>(kdTree);
     }
 
